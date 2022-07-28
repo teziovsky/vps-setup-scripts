fd0d4ba2a1fc6baecf73ec7be71e130c8fceac75
Jakub Soboczyński <teziovsky@gmail.com>
2022-07-28 14:43:15 +0200

    update: set ssh port

--- utils/vps_set_ssh_port.sh
+++ utils/vps_set_ssh_port.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # SET NEW SSH PORT NUMBER
 echo
-read -p "Enter SSH Port Number: " READ_SSH_PORT
+read -p -r "Enter SSH Port Number: " READ_SSH_PORT
 SSH_PORT=${READ_SSH_PORT:-10022}
 
 SSHD_FILE="/etc/ssh/sshd_config"
@@ -8,11 +10,13 @@ SSHD_BACKUP_FILE="/etc/ssh/sshd_config.backup"
 if [ ! -f "$SSHD_BACKUP_FILE" ]; then
     sudo cp $SSHD_FILE $SSHD_BACKUP_FILE
 else
-    echo "$SSHD_BACKUP_FILE" – Backup already exists!
+    echo "'$SSHD_BACKUP_FILE' – Backup already exists! 👌"
 fi
 
 if grep -q "#Port 22" "$SSHD_FILE"; then
     sudo sed -i "s/#Port 22/Port ${SSH_PORT}/g" $SSHD_FILE
+    elif grep -q "Port ${SSH_PORT}" "$SSHD_FILE"; then
+    echo "Port already set 👌"
 else
     echo "Port ${SSH_PORT}" | sudo tee -a $SSHD_FILE
 fi
