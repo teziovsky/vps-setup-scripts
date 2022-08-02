122458716a70a036a22373de9fe8af71e2653221
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-02 13:05:32 +0200

    feat: update scripts

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # IF NO SUDO, THEN EXIT
-source ./utils/check_sudo.sh
+if [ "$(id -u)" != "0" ]; then
+    echo "Musisz uruchomić ten skrypt jako root" 1>&2
+    echo "Spróbuj sudo $0"
+    exit 1
+fi
 
 # UPDATE SYSTEM
 sudo apt-get update -y && sudo apt-get upgrade -y
@@ -14,7 +18,5 @@ echo
 echo "Git installed 🎉"
 
 sudo git clone https://github.com/teziovsky/vps-setup-scripts.git /opt/vps-setup-scripts
-
-sudo chmod -R +x /opt/vps-setup-scripts
 
 (cd /opt/vps-setup-scripts && ./user_setup.sh)
--- user_setup.sh
+++ user_setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # IF NO SUDO, THEN EXIT
-source ./check_sudo.sh
+source ./utils/check_sudo.sh
 
 # UPDATE SYSTEM
-sudo apt-get update -y && sudo apt-get upgrade -y
-echo
-echo "System updated! 🎉"
 source ./utils/update_system.sh
+
+# INSTALL GIT
+source ./utils/install_git.sh
 
 # ADD USER
 source ./utils/add_user.sh
+
+# SET NEW SSH PORT
+source ./utils/set_ssh_port.sh
